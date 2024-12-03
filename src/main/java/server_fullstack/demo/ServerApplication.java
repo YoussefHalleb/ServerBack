c9b41fbce9e14c4bdfedcd7873da698cf894f411
Youssef <youssefhalleb20@gmail.com>
2024-12-03 21:35:46 +0100

    first commit

---
+++ src/main/java/server_fullstack/demo/ServerApplication.java
@@ -0,0 +1,52 @@
+package server_fullstack.demo;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import server_fullstack.demo.enumeration.Status;
+import server_fullstack.demo.model.Server;
+import server_fullstack.demo.repo.ServerRepo;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.155", "Kali Linux", "16 GB", "Personal PC", "http://localhost:8081/server/image/server1.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.154", "Ubuntu Linux", "8 GB", "Dell Tower", "http://localhost:8081/server/image/server2.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.153", "Red Hat Enterprise Linux", "16 GB", "Web Server", "http://localhost:8081/server/image/server3.png", Status.SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.1.152", "Sdd Linux", "32 GB", "Ultra PC", "http://localhost:8081/server/image/server4.png", Status.SERVER_UP));
+		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+	}
+
+}
+
+// The CommandLineRunner() method is annotated with @Bean, which means that Spring will manage the instance returned by this method as a bean.
